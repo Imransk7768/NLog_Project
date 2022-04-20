aca587e70f45d444ebbadd7273bbc26694b89fe3
Imransk7768 <imransk7768@gmail.com>
2022-04-20 08:05:21 +0530

    [Imran] Added: Implementing NLog Concept

---
+++ NLogProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NLog;
+
+namespace NLogProject
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Nlogger.");
+            AddNumbers add = new AddNumbers();
+            add.Sum(1, 2);
+        }
+    }
+}
